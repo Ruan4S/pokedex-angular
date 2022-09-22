0ae1ff429beb6973afdcd8c1caceef45eb1985ca
Ruan Araújo <ruands2002@gmail.com>
2022-09-21 21:24:29 -0300

    feat: pokedex finalizado

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -8,12 +8,28 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 })
 export class PokeListComponent implements OnInit {
   public getAllPokemons: any;
+  private setAllPokemons: any;
+  public apiError: boolean = false;
 
   constructor(private readonly pokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemons.subscribe((res) => {
-      this.getAllPokemons = res.results;
+    this.pokeApiService.apiListAllPokemons.subscribe({
+      next: (res) => {
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = res.results;
+      },
+      error: (err) => {
+        this.apiError = true;
+      },
     });
+  }
+
+  public search(value: string) {
+    const filter = this.setAllPokemons.filter((pokemon: any) => {
+      return !pokemon.name.indexOf(value.toLowerCase());
+    });
+
+    this.getAllPokemons = filter;
   }
 }
